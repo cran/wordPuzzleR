85cbe04173c0ac6a3a17e20bf5ee096fae2a9aab
Xiurui Zhu <zxr6@163.com>
2023-10-02 10:40:02 +0000

    version 0.1.0

---
+++ R/wordPuzzleR-package.R
@@ -0,0 +1,11 @@
+#' @examples
+#' # Run word puzzle game
+#' if (interactive() == TRUE) {
+#'   run_game()
+#' }
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
